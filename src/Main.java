6ff7eee788433bc7af85b5e2c1ac57dda92a3c0d
MariKidze <vashakidzemariy@gmail.com>
2023-03-13 15:12:00 +0400

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-  /*если клиент пополняет счет больше, че на 1000 рублей,*/
-  /*то оператор дарит ему по 1 рублю за каждые полные 100 рублей*/
+        /*если клиент пополняет счет больше, че на 1000 рублей,*/
+        /*то оператор дарит ему по 1 рублю за каждые полные 100 рублей*/
 
         int initialBalance = 700;
         int amountDeposit = 1200;
         int bonus;
 
-        if (amountDeposit  > 1000) {
-            bonus = amountDeposit/100;        }
-        else { bonus = 0; }
-        System.out.println( "Счет клиента:" + (initialBalance + amountDeposit + bonus) + "рублей");
+        if (amountDeposit > 1000) {
+            bonus = amountDeposit / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Счет клиента:" + (initialBalance + amountDeposit + bonus) + "рублей");
     }
 }
